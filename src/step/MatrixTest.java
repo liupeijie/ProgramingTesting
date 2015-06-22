03ec978b5a2aa492cc34461177bafe98a4c8f149
liupeijie <liupeijie525@gmail.com>
2015-06-22 16:08:31 +0900

    課題1完了

---
+++ src/step/MatrixTest.java
@@ -0,0 +1,16 @@
+package step;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by liupeijie on 西暦15/06/22.
+ */
+public class MatrixTest {
+    @Test
+    public void testMatrix(){
+
+        assertEquals(450.0, Matrix.matrix(3), 0);
+    }
+}
