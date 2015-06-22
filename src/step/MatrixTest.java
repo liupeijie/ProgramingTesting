aca2cae6c1234a33ec9f22caceca2ddc30c75b84
liupeijie <liupeijie525@gmail.com>
2015-06-22 16:14:02 +0900

    テスト完了

--- src/step/MatrixTest.java
+++ src/step/MatrixTest.java
@@ -12,5 +12,8 @@ public class MatrixTest {
     public void testMatrix(){
 
         assertEquals(450.0, Matrix.matrix(3), 0);
+        assertEquals(3680.0, Matrix.matrix(4), 0);
+        assertEquals(18250.0, Matrix.matrix(5), 0);
+   
     }
 }
