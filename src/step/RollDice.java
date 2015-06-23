9c65a5e6af1b2a6cd3547e2564b6e945106c7b7d
liupeijie <liupeijie525@gmail.com>
2015-06-23 15:25:19 +0900

    サイコロ全部の確率と偶数目の確率を調べてテストした

--- src/step/RollDice.java
+++ src/step/RollDice.java
@@ -6,25 +6,51 @@ package step;
 public class RollDice {
 
     public static boolean rollDice(int count , int No){
-        int num,i;
+        int i;
+        int num[] = new int[count];
         double temp[] = new double[7];
         double probability[] =new double[7];
         for(i=0; i<7; ++i) {
             temp[i]=0;
         }
         for(i=0; i<count; i++) {
-            num = (int) (Math.random() * 6) + 1;
-            if(num>6 || num <1)System.out.println(num);
-            temp[num]= temp[num]+1.0;
-            //System.out.println(num);
+            num[i] = (int) (Math.random() * 6) + 1;
+            if(num[i]>6 || num[i] <1)System.out.println(num);
+            temp[num[i]]= temp[num[i]]+1.0;
             //System.out.println(temp[num]);
         }
+
         for(i=1; i<7; ++i) {
             probability[i] = (temp[i] / count) - (1.0/6.0) ;
             //System.out.println(i+":"+probability[i]);
 
         }
+
         System.out.println(count+"回Rollして、"+No+"番出る確率と1/6の差は:"+probability[No]);
+        return -0.0015 < probability[No] && probability[No] < 0.0015;
+    }
+
+    //偏りを防ぐため、検証を行う
+    public static boolean checkRollDice(int count , int No){
+        int i ;
+        int num[] = new int[count];
+        double temp[] = new double[7];
+        double probability[] =new double[7];
+        
+        for(i=0 ; i< count;i++){
+            num[i] = (int) (Math.random() * 6) + 1;
+            if(num[i]>6 || num[i] <1)System.out.println(num[i]);
+            if(i % 2 ==0 ){
+                temp[num[i]]= temp[num[i]]+1.0;
+            }
+        }
+        count= count/2;
+        for(i=1; i<7; ++i) {
+            probability[i] = (temp[i] / count) - (1.0/6.0) ;
+            //System.out.println(i+":"+probability[i]);
+        }
+
+        System.out.println(count*2+"回Rollして、偶数目に"+No+"番出る確率と1/6の差は:"+probability[No]);
         return -0.0015 < probability[No] && probability[No] < 0.0015;
     }
 }
--- src/step/RollDiceTest.java
+++ src/step/RollDiceTest.java
@@ -11,12 +11,12 @@ public class RollDiceTest {
     @Test
     public void testRollDice(){
 
-        assertTrue(RollDice.rollDice(1000000,1));
+      /*  assertTrue(RollDice.rollDice(1000000,1));
         assertTrue(RollDice.rollDice(1000000,2));
         assertTrue(RollDice.rollDice(1000000,3));
         assertTrue(RollDice.rollDice(1000000,4));
         assertTrue(RollDice.rollDice(1000000,5));
-        assertTrue(RollDice.rollDice(1000000,6));
+        assertTrue(RollDice.rollDice(1000000,6));*/
 
         assertTrue(RollDice.rollDice(10000000,1));
         assertTrue(RollDice.rollDice(10000000,2));
@@ -24,6 +24,13 @@ public class RollDiceTest {
         assertTrue(RollDice.rollDice(10000000,4));
         assertTrue(RollDice.rollDice(10000000,5));
         assertTrue(RollDice.rollDice(10000000,6));
+
+        assertTrue(RollDice.checkRollDice(10000000, 1));
+        assertTrue(RollDice.checkRollDice(10000000, 2));
+        assertTrue(RollDice.checkRollDice(10000000, 3));
+        assertTrue(RollDice.checkRollDice(10000000, 4));
+        assertTrue(RollDice.checkRollDice(10000000,5));
+        assertTrue(RollDice.checkRollDice(10000000,6));
     }
 
 }
